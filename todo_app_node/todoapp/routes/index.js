c9155efc5f7b912f601cf11cf33866de41ef234b
code-yy <jiyeyouren523@gmail.com>
2022-06-29 17:36:03 +0900

    ✨ DBに保存

--- todo_app_node/todoapp/routes/index.js
+++ todo_app_node/todoapp/routes/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "$Yuto1124",
+  database: "todo_app",
+});
 
 let todos = [];
 
@@ -11,9 +19,22 @@ router.get("/", function (req, res, next) {
 });
 
 router.post("/", function (req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log("error connecting: " + err.stack);
+      return;
+    }
+    console.log("success");
+  });
   const todo = req.body.add;
-  todos.push(todo);
-  res.redirect("/");
+  connection.query(
+    `insert into tasks (user_id, content) values (1, '${todo}');`,
+    (error, results) => {
+      console.log(error);
+      console.log(todo);
+      res.redirect("/");
+    }
+  );
 });
 
 module.exports = router;
