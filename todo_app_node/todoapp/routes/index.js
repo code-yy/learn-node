b957f7c0fb0594ef2eeebb0bcea25f8d15cbd65e
code-yy <jiyeyouren523@gmail.com>
2022-06-30 17:45:52 +0900

    ✨ そのユーザーのみの絞り込みができるように

--- todo_app_node/todoapp/routes/index.js
+++ todo_app_node/todoapp/routes/index.js
@@ -2,25 +2,35 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../db/knex");
 
-router.get("/", function (req, res, next) {
+router.get("/", async function (req, res, next) {
   const isAuth = req.isAuthenticated();
-  knex("tasks")
-    .select("*")
-    .then(function (results) {
-      res.render("index", {
-        title: "ToDo App",
-        todos: results,
-        isAuth: isAuth,
+  const user = await req.user;
+
+  if (isAuth) {
+    knex("tasks")
+      .select("*")
+      .where({ user_id: user.id })
+      .then(function (results) {
+        res.render("index", {
+          title: "ToDo App",
+          todos: results,
+          isAuth: isAuth,
+        });
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render("index", {
+          title: "ToDo App",
+          isAuth: isAuth,
+          errorMessage: [err.sqlMessage],
+        });
       });
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.render("index", {
-        title: "ToDo App",
-        isAuth: isAuth,
-        errorMessage: [err.sqlMessage],
-      });
+  } else {
+    res.render("index", {
+      title: "ToDo App",
+      isAuth: isAuth,
     });
+  }
 });
 
 router.post("/", async function (req, res, next) {
