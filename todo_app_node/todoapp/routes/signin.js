bb06d6e5fed84fb46ef29f7cb29a39eb74955136
code-yy <jiyeyouren523@gmail.com>
2022-06-30 11:20:00 +0900

    ✨ passportを利用した認証を実装しよう

--- todo_app_node/todoapp/routes/signin.js
+++ todo_app_node/todoapp/routes/signin.js
@@ -1,41 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const knex = require("../db/knex");
+const passport = require("passport");
 
 router.get("/", function (req, res, next) {
+  const userId = req.session.userid;
+  console.log(req.session);
+  const isAuth = Boolean(userId);
   res.render("signin", {
     title: "Sign in",
+    isAuth: isAuth,
   });
 });
 
-router.post("/", function (req, res, next) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  knex("users")
-    .where({
-      name: username,
-      password: password,
-    })
-    .select("*")
-    .then((results) => {
-      if (results.length === 0) {
-        res.render("signin", {
-          title: "Sign in",
-          errorMessage: ["ユーザが見つかりません"],
-        });
-      } else {
-        req.session.userid = results[0].id;
-        res.redirect("/");
-      }
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.render("signin", {
-        title: "Sign in",
-        errorMessage: [err.sqlMessage],
-      });
-    });
-});
+router.post(
+  "/",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/signin",
+    failureFlash: true,
+  })
+);
 
 module.exports = router;
